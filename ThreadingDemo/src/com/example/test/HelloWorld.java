f9912c4d8e93af018ffdc596539d8bb2fc825a2b
Pankaj Arora <pankaj.arora307@gmail.com>
2016-09-09 15:39:23 +0530

    committed HelloWorld.java package modified

--- ThreadingDemo/src/com/example/test/HelloWorld.java
+++ ThreadingDemo/src/com/example/test/HelloWorld.java
@@ -1,11 +1,12 @@
 package com.example.test;
+
 class HelloWorld
 {
 
 	public static void main(String args[])
 	{
 	
-		System.out.print("Hello Hello");
+		System.out.println("Hello Hello");
 	}
 
 }
